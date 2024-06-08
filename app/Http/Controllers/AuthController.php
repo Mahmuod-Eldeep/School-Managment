9eca093f6e64e34ba42346bf8ce02c8648786521
AMhmoud <you@example.com>
2024-06-08 14:40:31 +0300

    [UPD IN RegisterRequest]

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -137,17 +137,17 @@ class AuthController extends Controller
 
     public function register(RegisterRequest $registerRequest)
     {
-
+        $userData = $registerRequest->only(['name', 'email', 'password', 'phoneNumber', 'classRoom', 'status']);
         if (Auth::user()->status !== 'Manager') {
             return response()->json([
                 'message' => 'Unauthorized'
             ], 403);
         }
 
-        $registerRequest['password'] = Hash::make($registerRequest['password']);
+        $userData['password'] = Hash::make($registerRequest['password']);
 
 
-        $user = User::create($registerRequest);
+        $user = User::create($userData);
 
 
         if ($registerRequest['status'] === 'Student') {
